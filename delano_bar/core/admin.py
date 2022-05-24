e7e7738075edb0f55732ac42f1d72083de2cb450
kostadinov1 <evgenikostadinov1987@gmail.com>
2022-05-24 16:41:54 +0300

    changed main html+css template

--- delano_bar/core/admin.py
+++ delano_bar/core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from delano_bar.core.models import Product, ProductCategory
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ProductCategory)
+class ProductCategoryAdmin(admin.ModelAdmin):
+    pass
--- delano_bar/core/views.py
+++ delano_bar/core/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+
+from delano_bar.core.models import Product
 
 
 class HomeView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['products'] = Product.objects.all()
+        return context
+
+
